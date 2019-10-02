7f58529bbe66dac7524152e730d270a20dfcf4f8
BranceA <brancealexander@gmail.com>
2019-10-02 14:58:19 -0500

    can add new contacts

--- src/FileUpdater.java
+++ src/FileUpdater.java
@@ -2,16 +2,18 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.nio.file.StandardOpenOption;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
 
 public class FileUpdater {
 
     // Path to data file
-    private static Path p = Paths.get("src", "data.txt");
+    protected static Path p = Paths.get("src", "data.txt");
 
     // Read datafile return List
-    public static List<String> readData() {
+    protected static List<String> readData() {
         List<String> lines = new ArrayList<>();
 
         try {
@@ -21,6 +23,39 @@ public class FileUpdater {
         }
 
         return lines;
+    }
+
+    protected static void addNewContact(){
+        List<String> lines = readData();
+        Scanner input = new Scanner(System.in);
+        System.out.println("Add contacts first and last name.");
+        String newName = input.nextLine();
+        System.out.format("What is %s's phone number?", newName);
+        String userInput = input.nextLine();
+        String formattedContact = "";
+        int phoneNumber = 0;
+        if(userInput.matches(".*\\d.*")){
+            phoneNumber = Integer.parseInt(userInput);
+        } else {
+            System.out.println("That isn't a phone number");
+            UserInterface.menu();
+        }
+
+        formattedContact += newName;
+        for(int i = 0; i <= (20 - newName.length()); i++){
+            formattedContact += " ";
+        }
+
+        formattedContact += "| " + phoneNumber;
+
+        lines.add(formattedContact);
+
+        try{
+            Files.write(p, lines);
+        } catch (IOException e){
+            e.printStackTrace();
+        }
+
     }
 
 }
--- src/FileUpdaterTest.java
+++ src/FileUpdaterTest.java
@@ -2,8 +2,9 @@ public class FileUpdaterTest {
 
     public static void main(String[] args) {
 
-        System.out.println("FileUpdater.readData() = " + FileUpdater.readData());
 
+        FileUpdater.addNewContact();
+        System.out.println("FileUpdater.readData() = " + FileUpdater.readData());
 
     }
 
